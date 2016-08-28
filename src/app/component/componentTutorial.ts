60cc1bee4e4910024a429e3fb2f7c150eea130e5
Elisa <hilprecht.elisa@googlemail.com>
2016-08-28 11:22:28 +0200

    Show first nested link on component tutorial activated.

--- src/app/component/componentTutorial.ts
+++ src/app/component/componentTutorial.ts
@@ -1,10 +1,13 @@
-import {Component, ElementRef, ViewChild, AfterViewChecked} from '@angular/core';
+import {Component, ElementRef, ViewChild, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'componentTutorial',
   styleUrls: ['./componentTutorial.css'],
   templateUrl: './componentTutorial.html'
 })
-export class ComponentTutorial{
+export class ComponentTutorial implements AfterViewInit{
 	@ViewChild('firstLink') firstLink: ElementRef;
+	ngAfterViewInit(){
+		this.firstLink.nativeElement.click();
+	}
 }
